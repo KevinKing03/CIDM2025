84e63d71948c5cd279defe66aa796ddb53b6c1d5
KevinKing03 <klpalomo1@buffs.wtamu.edu>
2025-02-03 17:43:34 -0600

    Add files via upload
    Q2 for HW2

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please input the first Num: ");
+        int num1 = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Please input the second Num: ");
+        int num2 = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Please input the third num: ");
+        int num3 = Convert.ToInt16(Console.ReadLine());
+
+        int smallest;
+
+        if (num1 <= num2)
+        {
+            if (num1 <= num3)
+            {
+                smallest = num1; 
+            }
+            else
+            {
+                smallest = num3; 
+            }
+        }
+        else
+        {
+            if (num2 <= num3)
+            {
+                smallest = num2; 
+            }
+            else
+            {
+                smallest = num3; 
+            }
+        }
+
+        Console.WriteLine("The smallest value is " + smallest);
+    }
+}
