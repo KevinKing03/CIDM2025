b07b98242704096bd76cccafc9d41622e56d3558
KevinKing03 <klpalomo1@buffs.wtamu.edu>
2025-02-03 17:53:46 -0600

    Homework2Bonus.cs

--- Homework2Bonus.cs
+++ Homework2Bonus.cs
@@ -3,11 +3,9 @@ class Program
 {
     static void Main()
     {
-        // Ask the user for a year input
         Console.Write("Please enter a year: ");
         int year = Convert.ToInt32(Console.ReadLine());
 
-        // Check if the year is a leap year
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
         {
             Console.WriteLine(year + " is a leap year.");
