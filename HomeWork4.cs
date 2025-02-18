487dd35bdc57f42c32bf1460a633257c02bf9588
KevinKing03 <klpalomo1@buffs.wtamu.edu>
2025-02-17 19:28:02 -0600

    Add files via upload

---
+++ HomeWork4.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static int GetLargestNumber(int num1, int num2)
+    {
+        return (num1 > num2) ? num1 : num2;
+    }
+
+    static void Main()
+    {
+        int a = 3, b = 5;
+        int largest = GetLargestNumber(a, b);
+
+        Console.WriteLine($"a = {a}; b = {b}");
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+}
