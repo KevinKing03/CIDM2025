dd16ce3fa3d134933eb4401a278e383d1c7d3791
KevinKing03 <klpalomo1@buffs.wtamu.edu>
2025-02-24 14:08:58 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Homework5;
+
+class Program
+{
+    static bool CheckAge(int birthYear)
+    {
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        return age >= 18;
+    }
+
+    static void CreateAccount()
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password1 = Console.ReadLine();
+
+        Console.Write("Re-enter password: ");
+        string password2 = Console.ReadLine();
+
+        Console.Write("Enter birth year: ");
+        int birthYear;
+        
+        while (!int.TryParse(Console.ReadLine(), out birthYear))
+        {
+            Console.Write("Invalid input. Please enter a valid birth year: ");
+        }
+
+        if (CheckAge(birthYear))
+        {
+            if (password1 == password2)
+            {
+                Console.WriteLine("Account is created successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Could not create an account.");
+        }
+    }
+
+    static void Main()
+    {
+        CreateAccount();
+    }
+}
